2a784d72340834900280f1a9371ff5c97ae63544
한상일 <87845749+Runor0624@users.noreply.github.com>
2024-02-09 11:18:36 +0900

    수정

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,14 +2,14 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-	title: 'Cart Page',
-	description: '장바구니 페이지 입니다.',
+  title: 'Cart Page',
+  description: '장바구니 페이지 입니다.',
 }
 
-export default function Page () {
-    return (
-        <>
-            <p>아마도 장바구니 페이지 초안</p>
-        </>
-    )
+export default function Page() {
+  return (
+    <>
+      <p>아마도 장바구니 페이지 초안</p>
+    </>
+  )
 }
