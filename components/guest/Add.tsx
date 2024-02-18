e2bd7f9e802cac18202d3b61887ada21426a3b88
한상일 <87845749+Runor0624@users.noreply.github.com>
2024-02-18 20:46:18 +0900

    [Modify] 방명록 관련 부분 수정

--- components/guest/Add.tsx
+++ components/guest/Add.tsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { GuestCardAddType } from '@/Types/Types'
+import { AiOutlinePlus } from 'react-icons/ai'
 
 export default function Add() {
   const router = useRouter()
@@ -31,20 +32,25 @@ export default function Add() {
     <>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className='w-1/2 border border-slate-300 flex flex-col justify-center items-center m-auto my-4'
+        className='w-1/2 flex flex-col justify-center items-center m-auto my-4'
       >
         <h1 className='text-center text-xl font-bold py-2'>방명록 추가</h1>
         <input
           placeholder='userId'
           {...register('userId', { required: true })}
-          className='m-2  w-full border border-slate-300 bg-white'
+          className='w-full sm:w-1/2 h-10 rounded-md my-2 text-blue-500 bg-white'
         />
         <input
           placeholder='comment'
           {...register('comment', { required: true })}
-          className='m-2  w-full border border-slate-300 bg-white'
+          className='w-full sm:w-1/2 h-10 rounded-md my-2 text-blue-500 bg-white'
         />
-        <button type='submit'>ADD</button>
+        <button
+          className='my-2 btn btn-md btn-success text-white w-full sm:w-1/2'
+          type='submit'
+        >
+          <AiOutlinePlus /> ADD
+        </button>
       </form>
     </>
   )
--- components/guest/card.tsx
+++ components/guest/card.tsx
@@ -1,20 +1,28 @@
 import { GuestCardType } from '@/Types/Types'
+import moment from 'moment'
+
 const Card = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL_KEY}/guest`, {
-    next: { revalidate: 5 },
+    next: { revalidate: 1 },
   })
   const Cards: GuestCardType[] = await res.json()
   return (
-    <>
+    <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-2'>
       {Cards.map((item: GuestCardType) => (
-        <section className='flex flex-row justify-center' key={item.id}>
-          <p>
-            작성자 : {item.userId}, 작성일 : {item.createDate}
-          </p>
-          <p>내용 : {item.comment}</p>
+        <section className='m-auto my-2 w-1/2' key={item.id}>
+          <div className='card w-96 bg-base-100'>
+            <div className='card-body items-center text-center'>
+              <h2 className='card-title'>작성자 : {item.userId}</h2>
+              <p>내용 : {item.comment}</p>
+              <p>
+                작성 시간 :{' '}
+                {moment(item.createDate).format('YYYY-MM-DD HH:mm:ss')}
+              </p>
+            </div>
+          </div>
         </section>
       ))}
-    </>
+    </div>
   )
 }
 export default Card
